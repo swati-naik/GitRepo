3f95a030c3047a4d82b8b3a97fd09547d784d5b6
swati naik <48499854+swati-naik@users.noreply.github.com>
2022-06-21 23:36:24 -0500

    Update welcome.java

--- src/welcome.java
+++ src/welcome.java
@@ -2,7 +2,7 @@
 public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello world");
+System.out.println("hello world - version2");
 	}
 
 }
